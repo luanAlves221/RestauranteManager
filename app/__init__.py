126f39c24a4f9398084cbc1fe18f71693a701f77
Luan Alves <luanlisboa220@gmail.com>
2025-02-07 00:37:17 -0300

    Corrigido problema na importação de Blueprints. Estruturado corretamente o banco de dados MySQL. Ajustado arquivo init.py para reconhecer a estrutura do projeto.

--- app/__init__.py
+++ app/__init__.py
@@ -14,5 +14,4 @@ def create_app():
 #    app.register_blueprint(reservations, url_prefix="/reservations")
     #app.register_blueprint(admin, url_prefix="/admin")
     app.register_blueprint(general, url_prefix="/")
-
     return app
